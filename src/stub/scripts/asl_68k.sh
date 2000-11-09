6a4d9475f2a288bcb6a44a2955e4ad37cf267d2b
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2000-11-09 23:40:28 +0000

    Updated for latest ASL release.
    committer: mfx <mfx> 973813228 +0000

--- src/stub/scripts/asl_68k.sh
+++ src/stub/scripts/asl_68k.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 set -e
 
-# wrapper for the ASL cross-assembler
-# http://john.ccac.rwth-aachen.de:8000/as/
+# wrapper for the ASL cross-assembler (version 1.42bld9)
+#   http://john.ccac.rwth-aachen.de:8000/as/
+#   http://john.ccac.rwth-aachen.de:8000/ftp/as/source/c_version/
 
 file="$1"
+test -f "$file" || exit 1
 
+ofile=`echo "$file" | sed 's/\.[a-z]*$/.o/'`
+
+# convert ' to " in dc.x statements
 perl -p -i -e '
-  s,\x27,",g if m,^\s*dc,;
+  s,\x27,",g if m,^\s*dc\.,;
 ' "$file"
 
-asl -cpu 68000 "$file"
+echo asl -q -xC -U -cpu 68000 -o "$ofile" -L "$file"
+     asl -q -xC -U -cpu 68000 -o "$ofile" -L "$file"
 
 exit 0
 
