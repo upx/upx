adcb3572422e054e60e9e79503fada1d7e8e6bc6
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2000-12-20 20:03:54 +0000

    Updated.
    committer: mfx <mfx> 977342634 +0000

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
-#define UPX_VERSION             0x011000        /* 01.10.00 */
-#define UPX_VERSION_STRING      "1.10"
+#define UPX_VERSION             0x011100        /* 01.11.00 */
+#define UPX_VERSION_STRING      "1.11"
 #define UPX_VERSION_DATE        "Dec 20th 2000"
