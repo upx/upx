14d22d29b19455052a72db1512fdd0ef95c6521d
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-07-16 16:18:36 +0000

    Big re-sync with UPX 1.2 branch.
    committer: mfx <mfx> 1026836316 +0000

--- src/stub/scripts/bin2h.pl
+++ src/stub/scripts/bin2h.pl
@@ -4,8 +4,8 @@
 #
 #  This file is part of the UPX executable compressor.
 #
-#  Copyright (C) 1996-2001 Markus Franz Xaver Johannes Oberhumer
-#  Copyright (C) 1996-2001 Laszlo Molnar
+#  Copyright (C) 1996-2002 Markus Franz Xaver Johannes Oberhumer
+#  Copyright (C) 1996-2002 Laszlo Molnar
 #  All Rights Reserved.
 #
 #  UPX and the UCL library are free software; you can redistribute them
@@ -23,9 +23,12 @@
 #  If not, write to the Free Software Foundation, Inc.,
 #  59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-#  Markus F.X.J. Oberhumer   Laszlo Molnar
-#  markus@oberhumer.com      ml1050@cdata.tvnet.hu
+#  Markus F.X.J. Oberhumer              Laszlo Molnar
+#  <mfx@users.sourceforge.net>          <ml1050@users.sourceforge.net>
 #
+
+
+use Compress::Zlib;
 
 
 $delim = $/;
@@ -62,8 +65,8 @@ print <<"EOF";
 
    This file is part of the UPX executable compressor.
 
-   Copyright (C) 1996-2001 Markus Franz Xaver Johannes Oberhumer
-   Copyright (C) 1996-2001 Laszlo Molnar
+   Copyright (C) 1996-2002 Markus Franz Xaver Johannes Oberhumer
+   Copyright (C) 1996-2002 Laszlo Molnar
    All Rights Reserved.
 
    UPX and the UCL library are free software; you can redistribute them
@@ -81,8 +84,8 @@ print <<"EOF";
    If not, write to the Free Software Foundation, Inc.,
    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-   Markus F.X.J. Oberhumer   Laszlo Molnar
-   markus\@oberhumer.com      ml1050\@cdata.tvnet.hu
+   Markus F.X.J. Oberhumer              Laszlo Molnar
+   <mfx\@users.sourceforge.net>          <ml1050\@users.sourceforge.net>
  */
 
 
@@ -91,8 +94,9 @@ EOF
 
 $s = $ident;
 $s =~ tr/a-z/A-Z/;
-$s .= "_ADLER32";
-printf("#define %s 0x%08x\n\n", $s, &adler32($data));
+printf("#define %s_ADLER32 0x%08x\n", $s, &adler32($data));
+printf("#define %s_CRC32   0x%08x\n", $s, &crc32($data));
+printf("\n");
 
 printf("unsigned char %s[%d] = {", $ident, $n);
 for ($i = 0; $i < $n; $i++) {
@@ -123,7 +127,7 @@ exit(0);
 # //
 # ************************************************************************/
 
-sub adler32 {
+sub adler32_OLD {
     local($d) = @_;
     local($n) = length($d);
     local($i);
--- src/stub/scripts/version.pl
+++ src/stub/scripts/version.pl
@@ -4,8 +4,8 @@
 #
 #  This file is part of the UPX executable compressor.
 #
-#  Copyright (C) 1996-2001 Markus Franz Xaver Johannes Oberhumer
-#  Copyright (C) 1996-2001 Laszlo Molnar
+#  Copyright (C) 1996-2002 Markus Franz Xaver Johannes Oberhumer
+#  Copyright (C) 1996-2002 Laszlo Molnar
 #  All Rights Reserved.
 #
 #  UPX and the UCL library are free software; you can redistribute them
@@ -23,8 +23,8 @@
 #  If not, write to the Free Software Foundation, Inc.,
 #  59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-#  Markus F.X.J. Oberhumer   Laszlo Molnar
-#  markus@oberhumer.com      ml1050@cdata.tvnet.hu
+#  Markus F.X.J. Oberhumer              Laszlo Molnar
+#  <mfx@users.sourceforge.net>          <ml1050@users.sourceforge.net>
 #
 
 
