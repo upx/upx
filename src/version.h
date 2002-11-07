f5a2641ea1fd23a109c89c34946f974bd90ddce6
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-11-07 16:59:41 +0000

    Changed version number and date.
    committer: mfx <mfx> 1036688381 +0000

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define UPX_VERSION_HEX         0x019001        /* 01.90.01 */
-#define UPX_VERSION_STRING      "1.90.1"
+#define UPX_VERSION_HEX         0x019000        /* 01.90.00 */
+#define UPX_VERSION_STRING      "1.90"
 #define UPX_VERSION_STRING4     "1.90"
-#define UPX_VERSION_DATE        "Oct 14th 2002"
+#define UPX_VERSION_DATE        "Nov 11th 2002"
