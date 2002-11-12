f3b64cfe789dac6e173a85246ddc012c8582c940
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-11-12 02:41:45 +0000

    Bumped version.
    committer: mfx <mfx> 1037068905 +0000

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define UPX_VERSION_HEX         0x019000        /* 01.90.00 */
-#define UPX_VERSION_STRING      "1.90"
-#define UPX_VERSION_STRING4     "1.90"
-#define UPX_VERSION_DATE        "Nov 11th 2002"
+#define UPX_VERSION_HEX         0x019100        /* 01.91.00 */
+#define UPX_VERSION_STRING      "1.91 beta"
+#define UPX_VERSION_STRING4     "1.91"
+#define UPX_VERSION_DATE        "Nov 12th 2002"
