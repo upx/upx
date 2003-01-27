c3040d1636a976e06a883e53b91c362226ab9e9a
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2003-01-27 18:53:31 +0000

    Updated.
    committer: mfx <mfx> 1043693611 +0000

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #define UPX_VERSION_HEX         0x019100        /* 01.91.00 */
 #define UPX_VERSION_STRING      "1.91 beta"
 #define UPX_VERSION_STRING4     "1.91"
-#define UPX_VERSION_DATE        "Jan 13th 2003"
+#define UPX_VERSION_DATE        "Jan 27th 2003"
