0e45021a5a8aa3fa3eb978fc5c5b93d182d7e486
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2004-05-12 01:14:22 +0000

    Bumped version-date.
    committer: mfx <mfx> 1084324462 +0000

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #define UPX_VERSION_HEX         0x019100        /* 01.91.00 */
 #define UPX_VERSION_STRING      "1.91 beta"
 #define UPX_VERSION_STRING4     "1.91"
-#define UPX_VERSION_DATE        "Apr 14th 2004"
+#define UPX_VERSION_DATE        "May 11th 2004"
