a3d95f239a8ee8631c3ef50ddf0488be7bb03d81
John Reiser <jreiser@BitWagon.com>
2005-11-09 20:08:32 +0000

    Generated .h header files for AMD x86_64 	fold_elf64amd.h l_lx_elf64amd.h
    committer: jreiser <jreiser> 1131566912 +0000

---
+++ src/stub/l_lx_elf64amd.h
@@ -0,0 +1,49 @@
+/* l_lx_elf64amd.h -- created from l_lx_elf64amd.bin, 253 (0xfd) bytes
+
+   This file is part of the UPX executable compressor.
+
+   Copyright (C) 1996-2005 Markus Franz Xaver Johannes Oberhumer
+   Copyright (C) 1996-2005 Laszlo Molnar
+   All Rights Reserved.
+
+   UPX and the UCL library are free software; you can redistribute them
+   and/or modify them under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.
+   If not, write to the Free Software Foundation, Inc.,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+   Markus F.X.J. Oberhumer              Laszlo Molnar
+   <mfx@users.sourceforge.net>          <ml1050@users.sourceforge.net>
+ */
+
+
+#define LINUX_ELF64AMD_LOADER_ADLER32 0x58207d7d
+#define LINUX_ELF64AMD_LOADER_CRC32   0x5d7a7590
+
+unsigned char linux_elf64amd_loader[253] = {
+232,242,  0,  0,  0, 85, 83, 81, 82, 72,  1,254, 86, 72,137,254,   /* 0x   0 */
+187,  0,  0,  0,128, 72,137,215, 72,131,205,255,232,121,  0,  0,   /* 0x  10 */
+  0,139, 30, 72,141,118,  4, 17,219,195,  1,219,116,243,195,164,   /* 0x  20 */
+255,210,114,251,184,  1,  0,  0,  0,235,  6,255,200,255,210, 17,   /* 0x  30 */
+192,255,210, 17,192,255,210,115,242, 49,201,131,232,  3,114, 18,   /* 0x  40 */
+193,224,  8,172,131,240,255,116, 78,209,248, 72, 99,232,114, 28,   /* 0x  50 */
+235,  4,255,210,114, 22,185,  1,  0,  0,  0,255,210,114, 13,255,   /* 0x  60 */
+210, 17,201,255,210,115,248,131,193,  2,235,  4,255,210, 17,201,   /* 0x  70 */
+ 72,129,253,  0,251,255,255,131,209,  2, 86, 72,141, 52, 47,243,   /* 0x  80 */
+164, 94,138,135,  0,  1,  0,  0,235,150, 90, 72,131,194,  9, 65,   /* 0x  90 */
+131,248,  8,117,  2,235,235, 89, 72,137,240, 72, 41,200, 90, 72,   /* 0x  a0 */
+ 41,215, 89,137, 57, 91, 93,195, 95, 41,246, 87,141, 86,  7,  3,   /* 0x  b0 */
+127,  4, 68,141, 86, 50,131,199, 11,185,  0,240,255,255, 41,207,   /* 0x  c0 */
+141, 70,  9, 33,207, 41,206, 81, 69, 41,192, 69, 41,201, 15,  5,   /* 0x  d0 */
+ 89, 57,200,114,  1,244, 94, 80,146,173,137,241,173,151,173, 68,   /* 0x  e0 */
+ 15,182,192,135,254,255,229, 93,232,187,255,255,255                /* 0x  f0 */
+};
