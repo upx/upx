023c06e178e6d0763447b2bcf28e84f1bebc79d3
John Reiser <jreiser@BitWagon.com>
2005-11-09 20:07:15 +0000

    Support for 64-bit AMD x86_64 	Makefile Added Files: 	a_lx_elf64.c amd_bxx.S amd_d_nrv2e.S amd_regs.h 	fold_elf64amd.S l_lx_elf64amd.S l_lx_elf64amd.lds
    committer: jreiser <jreiser> 1131566835 +0000

---
+++ src/stub/amd_regs.h
@@ -0,0 +1,15 @@
+/* NOTE: THE FIRST ARGUMENT IS arg1, NOT arg0. */
+#define arg1  rdi
+#define arg1l  edi
+#define arg2  rsi
+#define arg2l  esi
+#define arg3  rdx
+#define arg3l  edx
+#define arg4  rcx
+#define arg4l  ecx
+#define sys4   r10  # 4th arg to syscall is not in %rcx
+#define sys4l  r10d
+#define arg5  r8
+#define arg5l  r8d
+#define arg6  r9
+#define arg6l  r9d
