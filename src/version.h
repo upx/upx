23a6d326e8ffe157e9b30c06ff09ca3778720133
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-02-13 14:15:29 +0000

    Bump tentative release date. Update READMEs.
    committer: mfx <mfx> 1139840129 +0000

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define UPX_VERSION_HEX         0x019400        /* 01.94.00 */
 #define UPX_VERSION_STRING      "1.94 beta"
 #define UPX_VERSION_STRING4     "1.94"
-#define UPX_VERSION_DATE        "Feb 13th 2006"
+#define UPX_VERSION_DATE        "Mar 13th 2006"
 #define UPX_VERSION_YEAR        "2006"
