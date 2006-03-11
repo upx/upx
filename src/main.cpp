fcc22a0aa359234aa9c83ffe1940bad1a412c477
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-03-11 13:53:15 +0000

    Added UPX_VERSION_DATE_ISO.
    committer: mfx <mfx> 1142085195 +0000

--- src/main.cpp
+++ src/main.cpp
@@ -1114,6 +1114,7 @@ void upx_sanity_check(void)
     assert(memcmp(UPX_VERSION_STRING4, UPX_VERSION_STRING, 4) == 0);
     COMPILE_TIME_ASSERT(sizeof(UPX_VERSION_YEAR) == 4 + 1)
     assert(strlen(UPX_VERSION_YEAR) == 4);
+    assert(memcmp(UPX_VERSION_DATE_ISO, UPX_VERSION_YEAR, 4) == 0);
     assert(memcmp(UPX_VERSION_DATE + strlen(UPX_VERSION_DATE) - 4, UPX_VERSION_YEAR, 4) == 0);
 
 #if 1
--- src/version.h
+++ src/version.h
@@ -2,4 +2,5 @@
 #define UPX_VERSION_STRING      "1.94 beta"
 #define UPX_VERSION_STRING4     "1.94"
 #define UPX_VERSION_DATE        "Mar 11th 2006"
+#define UPX_VERSION_DATE_ISO    "2006-03-11"
 #define UPX_VERSION_YEAR        "2006"
