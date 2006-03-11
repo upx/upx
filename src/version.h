88d5798f457079397189d3815d429a6fab6ea635
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-03-11 15:22:56 +0000

    Bumped versions for starting 1.95 development.
    committer: mfx <mfx> 1142090576 +0000

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define UPX_VERSION_HEX         0x019400        /* 01.94.00 */
-#define UPX_VERSION_STRING      "1.94 beta"
-#define UPX_VERSION_STRING4     "1.94"
-#define UPX_VERSION_DATE        "Mar 11th 2006"
-#define UPX_VERSION_DATE_ISO    "2006-03-11"
+#define UPX_VERSION_HEX         0x019500        /* 01.95.00 */
+#define UPX_VERSION_STRING      "1.95 beta"
+#define UPX_VERSION_STRING4     "1.95"
+#define UPX_VERSION_DATE        "Mar 12th 2006"
+#define UPX_VERSION_DATE_ISO    "2006-03-12"
 #define UPX_VERSION_YEAR        "2006"
