c82b50b2124637ba9f7c39d18c0e5af07c0a123f
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-03-17 15:40:24 +0000

    Bumped tentative release date.
    committer: mfx <mfx> 1142610024 +0000

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x019500        /* 01.95.00 */
 #define UPX_VERSION_STRING      "1.95 beta"
 #define UPX_VERSION_STRING4     "1.95"
-#define UPX_VERSION_DATE        "Mar 12th 2006"
-#define UPX_VERSION_DATE_ISO    "2006-03-12"
+#define UPX_VERSION_DATE        "Mar 18th 2006"
+#define UPX_VERSION_DATE_ISO    "2006-03-18"
 #define UPX_VERSION_YEAR        "2006"
