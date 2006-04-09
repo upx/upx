1879ccd27de0da5336f820bd9f94ea5aff7b15cd
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-04-09 12:38:12 +0000

    Next version will be 2.00.
    committer: mfx <mfx> 1144586292 +0000

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define UPX_VERSION_HEX         0x019500        /* 01.95.00 */
-#define UPX_VERSION_STRING      "1.95 beta"
-#define UPX_VERSION_STRING4     "1.95"
-#define UPX_VERSION_DATE        "Apr 9th 2006"
-#define UPX_VERSION_DATE_ISO    "2006-04-09"
+#define UPX_VERSION_HEX         0x020000        /* 02.00.00 */
+#define UPX_VERSION_STRING      "2.00"
+#define UPX_VERSION_STRING4     "2.00"
+#define UPX_VERSION_DATE        "Apr 10th 2006"
+#define UPX_VERSION_DATE_ISO    "2006-04-10"
 #define UPX_VERSION_YEAR        "2006"
