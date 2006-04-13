f700273624f4b16ffadff169838b1cde1110afc6
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-04-13 16:10:28 +0000

    Really bump the release date.
    committer: mfx <mfx> 1144944628 +0000

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x020000        /* 02.00.00 */
 #define UPX_VERSION_STRING      "2.00"
 #define UPX_VERSION_STRING4     "2.00"
-#define UPX_VERSION_DATE        "Apr 13th 2006"
-#define UPX_VERSION_DATE_ISO    "2006-04-13"
+#define UPX_VERSION_DATE        "Apr 27th 2006"
+#define UPX_VERSION_DATE_ISO    "2006-04-27"
 #define UPX_VERSION_YEAR        "2006"
