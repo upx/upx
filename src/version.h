89cfa22c3ff61f0a105b497aa5cfe29ccba8ee75
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-06-06 17:36:34 +0200

    Final cleanups for 2.01.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x020100        /* 02.01.00 */
 #define UPX_VERSION_STRING      "2.01"
 #define UPX_VERSION_STRING4     "2.01"
-#define UPX_VERSION_DATE        "Apr 28th 2006"
-#define UPX_VERSION_DATE_ISO    "2006-04-28"
+#define UPX_VERSION_DATE        "Jun 06th 2006"
+#define UPX_VERSION_DATE_ISO    "2006-06-06"
 #define UPX_VERSION_YEAR        "2006"
