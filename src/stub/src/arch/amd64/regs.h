47f946cd65fb7c6e96798a07ff26258fde94b1c6
John Reiser <jreiser@BitWagon.com>
2006-10-01 14:25:14 -0700

    fix the comments

--- src/stub/src/arch/amd64/regs.h
+++ src/stub/src/arch/amd64/regs.h
@@ -5,9 +5,9 @@
 #define arg2l  esi
 #define arg3  rdx
 #define arg3l  edx
-#define arg4  rcx
+#define arg4  rcx   /* clobbered by syscall */
 #define arg4l  ecx
-#define sys4   r10  # 4th arg to syscall is not in %rcx
+#define sys4   r10  /* 4th arg to syscall is not in %rcx */
 #define sys4l  r10d
 #define arg5  r8
 #define arg5l  r8d
