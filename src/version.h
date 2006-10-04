610fd7561bcc24041c75e3bb3ab921d8f4690a62
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2006-10-04 09:26:34 +0200

    Bump tentative release date. Update NEWS.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x029000        /* 02.90.00 */
-#define UPX_VERSION_STRING      "2.90beta1"
+#define UPX_VERSION_STRING      "2.90"
 #define UPX_VERSION_STRING4     "2.90"
-#define UPX_VERSION_DATE        "Jun 22th 2006"
-#define UPX_VERSION_DATE_ISO    "2006-06-22"
+#define UPX_VERSION_DATE        "Oct 8th 2006"
+#define UPX_VERSION_DATE_ISO    "2006-10-08"
 #define UPX_VERSION_YEAR        "2006"
