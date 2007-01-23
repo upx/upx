70632e554132348a6032c3826e39961b1528e594
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2007-01-23 22:11:23 +0100

    Bump release date.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x029200        /* 02.92.00 */
 #define UPX_VERSION_STRING      "2.92 beta"
 #define UPX_VERSION_STRING4     "2.92"
-#define UPX_VERSION_DATE        "Jan 14th 2007"
-#define UPX_VERSION_DATE_ISO    "2007-01-14"
+#define UPX_VERSION_DATE        "Jan 23rd 2007"
+#define UPX_VERSION_DATE_ISO    "2007-01-23"
 #define UPX_VERSION_YEAR        "2007"
