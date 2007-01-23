39f5eb753fd18e9e18139c13a2da5aa445c8305a
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2007-01-23 22:17:02 +0100

    Bump version for next release.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define UPX_VERSION_HEX         0x029200        /* 02.92.00 */
-#define UPX_VERSION_STRING      "2.92 beta"
-#define UPX_VERSION_STRING4     "2.92"
-#define UPX_VERSION_DATE        "Jan 23rd 2007"
-#define UPX_VERSION_DATE_ISO    "2007-01-23"
+#define UPX_VERSION_HEX         0x029300        /* 02.93.00 */
+#define UPX_VERSION_STRING      "2.93 beta"
+#define UPX_VERSION_STRING4     "2.93"
+#define UPX_VERSION_DATE        "Jan 24th 2007"
+#define UPX_VERSION_DATE_ISO    "2007-01-24"
 #define UPX_VERSION_YEAR        "2007"
