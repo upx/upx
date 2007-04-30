530e6d9196d48bef559ab017124bbb6f484dbed5
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2007-04-30 19:34:26 +0200

    Bump version for next release.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define UPX_VERSION_HEX         0x030000        /* 03.00.00 */
-#define UPX_VERSION_STRING      "3.00"
-#define UPX_VERSION_STRING4     "3.00"
-#define UPX_VERSION_DATE        "Apr 27th 2007"
-#define UPX_VERSION_DATE_ISO    "2007-04-27"
+#define UPX_VERSION_HEX         0x030100        /* 03.01.00 */
+#define UPX_VERSION_STRING      "3.01"
+#define UPX_VERSION_STRING4     "3.01"
+#define UPX_VERSION_DATE        "Apr 30th 2007"
+#define UPX_VERSION_DATE_ISO    "2007-04-30"
 #define UPX_VERSION_YEAR        "2007"
