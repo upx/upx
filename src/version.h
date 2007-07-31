ace2359e7bf8ca2b3558dee0e9df2836be3ce544
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2007-07-31 15:06:53 +0200

    Bump tentative release date.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x030100        /* 03.01.00 */
 #define UPX_VERSION_STRING      "3.01"
 #define UPX_VERSION_STRING4     "3.01"
-#define UPX_VERSION_DATE        "Apr 30th 2007"
-#define UPX_VERSION_DATE_ISO    "2007-04-30"
+#define UPX_VERSION_DATE        "Jul 31st 2007"
+#define UPX_VERSION_DATE_ISO    "2007-07-31"
 #define UPX_VERSION_YEAR        "2007"
