a6b04f426effbf1d2ac5dde89a06520e97070170
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2008-01-02 09:47:52 +0100

    Bump version year.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x030300        /* 03.03.00 */
 #define UPX_VERSION_STRING      "3.03"
 #define UPX_VERSION_STRING4     "3.03"
-#define UPX_VERSION_DATE        "Dec 17th 2007"
-#define UPX_VERSION_DATE_ISO    "2007-12-17"
-#define UPX_VERSION_YEAR        "2007"
+#define UPX_VERSION_DATE        "Jan 2nd 2008"
+#define UPX_VERSION_DATE_ISO    "2008-01-02"
+#define UPX_VERSION_YEAR        "2008"
