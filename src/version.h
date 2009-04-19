9694ff433fe71479763c038008aac61111c64fdd
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2009-04-19 20:14:24 +0200

    Fix grammar in version date.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x030400        /* 03.04.00 */
 #define UPX_VERSION_STRING      "3.04"
 #define UPX_VERSION_STRING4     "3.04"
-#define UPX_VERSION_DATE        "Apr 27st 2009"
+#define UPX_VERSION_DATE        "Apr 27th 2009"
 #define UPX_VERSION_DATE_ISO    "2009-04-27"
 #define UPX_VERSION_YEAR        "2009"
