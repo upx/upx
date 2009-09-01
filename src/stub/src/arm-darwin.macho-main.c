ae0ab5025e74ec56c388d5a22f809eb74b3ea65b
John Reiser <jreiser@BitWagon.com>
2009-09-01 07:21:18 -0700

    fix debug mixup

--- src/stub/src/arm-darwin.macho-main.c
+++ src/stub/src/arm-darwin.macho-main.c
@@ -301,8 +301,8 @@ typedef union {
 
 #define MAP_FIXED     0x10
 #define MAP_PRIVATE   0x02
-//#define MAP_ANON    0x1000
-#define MAP_ANON  0x20  // x86 DEBUG ONLY
+#define MAP_ANON    0x1000
+//#define MAP_ANON  0x20  // x86 DEBUG ONLY
 #define PROT_READ      1
 #define PROT_WRITE     2
 #define PROT_EXEC      4
