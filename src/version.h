bd15ef0a0c33f16434cbc3b4e2aa9ae5db1f26b1
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2009-09-21 05:44:19 +0200

    Bump tentative 3.04 release date.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x030400        /* 03.04.00 */
 #define UPX_VERSION_STRING      "3.04"
 #define UPX_VERSION_STRING4     "3.04"
-#define UPX_VERSION_DATE        "Apr 27th 2009"
-#define UPX_VERSION_DATE_ISO    "2009-04-27"
+#define UPX_VERSION_DATE        "Sep 25th 2009"
+#define UPX_VERSION_DATE_ISO    "2009-09-25"
 #define UPX_VERSION_YEAR        "2009"
