fe1a0ab37192dbda9d986471daa967b478b9f06c
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2010-03-06 16:50:17 +0100

    Bump tentative release date.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x030500        /* 03.05.00 */
 #define UPX_VERSION_STRING      "3.05"
 #define UPX_VERSION_STRING4     "3.05"
-#define UPX_VERSION_DATE        "Sep 28th 2009"
-#define UPX_VERSION_DATE_ISO    "2009-09-28"
-#define UPX_VERSION_YEAR        "2009"
+#define UPX_VERSION_DATE        "Apr 27th 2010"
+#define UPX_VERSION_DATE_ISO    "2010-04-27"
+#define UPX_VERSION_YEAR        "2010"
