955c31b16513d2c17060fdbc02edd8a4afd76ff1
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2010-09-04 13:11:59 +0200

    Prepare release.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x030600        /* 03.06.00 */
 #define UPX_VERSION_STRING      "3.06"
 #define UPX_VERSION_STRING4     "3.06"
-#define UPX_VERSION_DATE        "Apr 28th 2010"
-#define UPX_VERSION_DATE_ISO    "2010-04-28"
+#define UPX_VERSION_DATE        "Sep 04th 2010"
+#define UPX_VERSION_DATE_ISO    "2010-09-04"
 #define UPX_VERSION_YEAR        "2010"
