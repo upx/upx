cf283069971be966a73ef80753dc5299220d7183
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2011-12-13 01:30:00 +0100

    Post-release version bump.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define UPX_VERSION_HEX         0x030800        /* 03.08.00 */
-#define UPX_VERSION_STRING      "3.08"
-#define UPX_VERSION_STRING4     "3.08"
-#define UPX_VERSION_DATE        "Dec 12th 2011"
-#define UPX_VERSION_DATE_ISO    "2011-12-12"
+#define UPX_VERSION_HEX         0x030900        /* 03.09.00 */
+#define UPX_VERSION_STRING      "3.09"
+#define UPX_VERSION_STRING4     "3.09"
+#define UPX_VERSION_DATE        "Dec 13th 2011"
+#define UPX_VERSION_DATE_ISO    "2011-12-13"
 #define UPX_VERSION_YEAR        "2011"
