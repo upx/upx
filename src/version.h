84996e8cb6637aa7d72c8f60e4319ae5ee0a9211
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2013-02-12 10:32:11 +0100

    Bump tentative release date.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x030900        /* 03.09.00 */
 #define UPX_VERSION_STRING      "3.09"
 #define UPX_VERSION_STRING4     "3.09"
-#define UPX_VERSION_DATE        "Jan 1st 2012"
-#define UPX_VERSION_DATE_ISO    "2012-01-01"
-#define UPX_VERSION_YEAR        "2012"
+#define UPX_VERSION_DATE        "Feb 17th 2013"
+#define UPX_VERSION_DATE_ISO    "2013-02-17"
+#define UPX_VERSION_YEAR        "2013"
