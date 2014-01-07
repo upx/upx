a26c982e9348358b9d84d90cb98c5b13dc46c04e
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2014-01-07 23:05:10 +0100

    Welcome 2014.

--- src/stub/scripts/UNUSED/setfold.pl
+++ src/stub/scripts/UNUSED/setfold.pl
@@ -4,7 +4,7 @@
 #
 #  This file is part of the UPX executable compressor.
 #
-#  Copyright (C) 2000-2013 John F. Reiser
+#  Copyright (C) 2000-2014 John F. Reiser
 #  All Rights Reserved.
 #
 #  UPX and the UCL library are free software; you can redistribute them
