4551937812562c3c1a264f2f834d41a1b6d78212
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2015-01-03 00:26:48 +0100

    Bump version year.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define UPX_VERSION_HEX         0x035c00        /* 03.91.00 */
+#define UPX_VERSION_HEX         0x035c00        /* 03.92.00 */
 #define UPX_VERSION_STRING      "3.92"
 #define UPX_VERSION_STRING4     "3.92"
-#define UPX_VERSION_DATE        "Mar 30th 2014"
-#define UPX_VERSION_DATE_ISO    "2014-03-30"
-#define UPX_VERSION_YEAR        "2014"
+#define UPX_VERSION_DATE        "Mar 30th 2015"
+#define UPX_VERSION_DATE_ISO    "2015-03-30"
+#define UPX_VERSION_YEAR        "2015"
