2781e37ec08ca320250d7e4a0b0f0e0c4e75528e
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2016-09-22 14:05:08 +0200

    Mark version as "BETA".

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define UPX_VERSION_HEX         0x035c00        /* 03.92.00 */
-#define UPX_VERSION_STRING      "3.92"
+#define UPX_VERSION_STRING      "3.92-BETA"
 #define UPX_VERSION_STRING4     "3.92"
 #define UPX_VERSION_DATE        "Sep 22nd 2016"
 #define UPX_VERSION_DATE_ISO    "2016-09-22"
