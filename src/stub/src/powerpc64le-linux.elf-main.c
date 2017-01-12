353c41f68bd2932d4a628ea06418c8b48422f30b
Thierry Fauck <tfauck@free.fr>
2017-01-12 12:03:16 +0100

    Explicitely specify cmp instructions to be on 64bits for ppc64le     Pointer to function in f_expand is 64bits on ppc64le     For ppc64le unpack may result in +1 or +3 offset
    Signed-off-by: Thierry Fauck <tfauck@free.fr>
    
    	modified:   src/stub/scripts/xstrip.py
    	modified:   src/stub/src/arch/powerpc/32/bxx.S
    	modified:   src/stub/src/arch/powerpc/64le/bxx.S
    	modified:   src/stub/src/arch/powerpc/64le/lzma_d.S
    	modified:   src/stub/src/arch/powerpc/64le/nrv2b_d.S
    	modified:   src/stub/src/arch/powerpc/64le/nrv2d_d.S
    	modified:   src/stub/src/arch/powerpc/64le/nrv2e_d.S
    	modified:   src/stub/src/powerpc64le-darwin.dylib-entry.S
    	modified:   src/stub/src/powerpc64le-darwin.macho-entry.S
    	modified:   src/stub/src/powerpc64le-darwin.macho-fold.S
    	modified:   src/stub/src/powerpc64le-linux.elf-entry.S
    	modified:   src/stub/src/powerpc64le-linux.elf-fold.S
    	modified:   src/stub/src/powerpc64le-linux.elf-main.c
    	modified:   src/stub/src/powerpc64le-linux.kernel.vmlinux.S

--- src/stub/src/powerpc64le-linux.elf-main.c
+++ src/stub/src/powerpc64le-linux.elf-main.c
@@ -98,7 +98,7 @@ typedef void f_unfilter(
 );
 typedef int f_expand(
     const nrv_byte *, nrv_uint,
-          nrv_byte *, nrv_uint *, unsigned );
+          nrv_byte *, long *, unsigned );
 
 static void
 unpackExtent(
@@ -136,7 +136,7 @@ ERR_LAB
         //   assert(h.sz_cpr > 0 && h.sz_cpr <= blocksize);
 
         if (h.sz_cpr < h.sz_unc) { // Decompress block
-            nrv_uint out_len = h.sz_unc;  // EOF for lzma
+            long out_len = h.sz_unc;  // EOF for lzma
             int const j = (*f_decompress)((const unsigned char *)xi->buf, h.sz_cpr,
                 (unsigned char *)xo->buf, &out_len, h.b_method);
             if (j != 0 || out_len != (nrv_uint)h.sz_unc)
