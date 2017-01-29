f69199909f0902de2e466d7639d45db0ec8b1674
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2017-01-29 14:05:00 +0100

    Post-release version bump.

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
-#define UPX_VERSION_HEX         0x035d00        /* 03.93.00 */
-#define UPX_VERSION_STRING      "3.93"
-#define UPX_VERSION_STRING4     "3.93"
-#define UPX_VERSION_DATE        "Jan 29th 2017"
-#define UPX_VERSION_DATE_ISO    "2017-01-29"
+#define UPX_VERSION_HEX         0x035e00        /* 03.94.00 */
+#define UPX_VERSION_STRING      "3.94"
+#define UPX_VERSION_STRING4     "3.94"
+#define UPX_VERSION_DATE        "Jan 30th 2017"
+#define UPX_VERSION_DATE_ISO    "2017-01-30"
 #define UPX_VERSION_YEAR        "2017"
 
 /* vim:set ts=4 sw=4 et: */
