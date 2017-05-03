304d848f53ffe3fdab38be6fe0f8a47a0b44adba
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2017-05-03 13:49:22 +0200

    Bump version date.

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #define UPX_VERSION_HEX         0x035e00        /* 03.94.00 */
 #define UPX_VERSION_STRING      "3.94"
 #define UPX_VERSION_STRING4     "3.94"
-#define UPX_VERSION_DATE        "Jan 30th 2017"
-#define UPX_VERSION_DATE_ISO    "2017-01-30"
+#define UPX_VERSION_DATE        "May 3rd 2017"
+#define UPX_VERSION_DATE_ISO    "2017-05-03"
 #define UPX_VERSION_YEAR        "2017"
 
 /* vim:set ts=4 sw=4 et: */
