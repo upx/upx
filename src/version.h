d985a4c250cb184735d1cfb297e0732c7bd49acf
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2017-05-12 15:28:47 +0200

    Post-release version bump.

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
-#define UPX_VERSION_HEX         0x035e00        /* 03.94.00 */
-#define UPX_VERSION_STRING      "3.94"
-#define UPX_VERSION_STRING4     "3.94"
-#define UPX_VERSION_DATE        "May 12th 2017"
+#define UPX_VERSION_HEX         0x035f00        /* 03.95.00 */
+#define UPX_VERSION_STRING      "3.95"
+#define UPX_VERSION_STRING4     "3.95"
+#define UPX_VERSION_DATE        "May 13th 2017"
 #define UPX_VERSION_DATE_ISO    "2017-05-12"
 #define UPX_VERSION_YEAR        "2017"
 
