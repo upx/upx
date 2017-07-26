5b5454443834e72c1c3a80589b44c22697427ea8
John Reiser <jreiser@BitWagon.com>
2017-07-26 11:16:02 -0700

    Fix powerpc64 (BIG_ENDIAN and comments)
    	modified:   stub/src/arch/powerpc/64/lzma_d.S
    	modified:   stub/src/arch/powerpc/64le/lzma_d-common.S
    	modified:   stub/src/powerpc64-linux.elf-main.c
    	modified:   stub/src/powerpc64le-darwin.dylib-entry.S
    	modified:   stub/src/powerpc64le-darwin.macho-entry.S
    	modified:   stub/src/powerpc64le-linux.elf-entry.S
    	modified:   stub/src/powerpc64le-linux.elf-fold.S
    	modified:   stub/src/powerpc64le-linux.elf-main.c
    	modified:   stub/src/powerpc64le-linux.kernel.vmlinux.S
    
    	plus stub/powerpc64*entry.h and .dump

--- src/stub/src/powerpc64-linux.elf-main.c
+++ src/stub/src/powerpc64-linux.elf-main.c
@@ -1,2 +1,4 @@
+#ifndef BIG_ENDIAN  //{
 #define BIG_ENDIAN 1
+#endif  //}
 #include "powerpc64le-linux.elf-main.c"
--- src/stub/src/powerpc64le-linux.elf-main.c
+++ src/stub/src/powerpc64le-linux.elf-main.c
@@ -1,2 +1,4 @@
-#define BIG_ENDIAN 1
+#ifndef BIG_ENDIAN  //{
+#define BIG_ENDIAN 0
+#endif  //}
 #include "amd64-linux.elf-main.c"
