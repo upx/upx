e853119c775dc64d45c31198372bdc7eb3121005
John Reiser <jreiser@BitWagon.com>
2018-01-06 09:20:46 -0800

    canonicalize bool in OR
    MSVC error C4805: '|' : unsafe mix of type 'bool' and type 'unsigned int' in operation
    	modified:   p_lx_elf.cpp

--- src/p_lx_elf.cpp
+++ src/p_lx_elf.cpp
@@ -608,7 +608,7 @@ PackLinuxElf::addStubEntrySections(Filter const *)
 
 void PackLinuxElf::defineSymbols(Filter const *)
 {
-    linker->defineSymbol("O_BINFO", opt->o_unix.is_ptinterp | o_binfo);
+    linker->defineSymbol("O_BINFO", (!!opt->o_unix.is_ptinterp) | o_binfo);
 }
 
 void PackLinuxElf32::defineSymbols(Filter const *ft)
