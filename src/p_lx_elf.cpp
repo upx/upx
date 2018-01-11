df94f4288a9faaddd2436002d753023e8d012a6e
John Reiser <jreiser@BitWagon.com>
2018-01-11 12:09:43 -0800

    picky, picky
    	modified:   p_lx_elf.cpp

--- src/p_lx_elf.cpp
+++ src/p_lx_elf.cpp
@@ -3772,7 +3772,7 @@ PackLinuxElf32::check_pt_dynamic(Elf32_Phdr const *const phdr)
 {
     unsigned t = get_te32(&phdr->p_offset), s = sizeof(Elf32_Dyn) + t;
     unsigned filesz = get_te32(&phdr->p_filesz), memsz = get_te32(&phdr->p_memsz);
-    if (s < t || file_size < s
+    if (s < t || file_size < (off_t)s
     ||  (3 & t) || (7 & (filesz | memsz))  // .balign 4; 8==sizeof(Elf32_Dyn)
     ||  filesz < sizeof(Elf32_Dyn)
     ||  memsz  < sizeof(Elf32_Dyn)
