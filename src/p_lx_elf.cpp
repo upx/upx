969efe687db7c14effbded52967d474b1c2935f5
John Reiser <jreiser@BitWagon.com>
2018-01-11 12:01:00 -0800

    MSVC pecadillos
    	modified:   p_lx_elf.cpp
    	modified:   pefile.cpp

--- src/p_lx_elf.cpp
+++ src/p_lx_elf.cpp
@@ -3843,7 +3843,7 @@ PackLinuxElf64::check_pt_dynamic(Elf64_Phdr const *const phdr)
 {
     upx_uint64_t t = get_te64(&phdr->p_offset), s = sizeof(Elf64_Dyn) + t;
     upx_uint64_t filesz = get_te64(&phdr->p_filesz), memsz = get_te64(&phdr->p_memsz);
-    if (s < t || (uint64_t)file_size < s
+    if (s < t || (upx_uint64_t)file_size < s
     ||  (7 & t) || (0xf & (filesz | memsz))  // .balign 8; 16==sizeof(Elf64_Dyn)
     ||  filesz < sizeof(Elf64_Dyn)
     ||  memsz  < sizeof(Elf64_Dyn)
