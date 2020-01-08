b5b1f06a11133a988bcff777da09a865ac523718
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2020-01-08 06:04:21 +0100

    Avoid some compilation warnings.

--- src/compress_lzma.cpp
+++ src/compress_lzma.cpp
@@ -30,6 +30,12 @@
 #include "compress.h"
 #include "mem.h"
 
+#if (ACC_CC_CLANG)
+#  pragma clang diagnostic ignored "-Wshadow"
+#endif
+#if (ACC_CC_GNUC >= 0x040200)
+#  pragma GCC diagnostic ignored "-Wshadow"
+#endif
 #if (ACC_CC_MSC)
 #  pragma warning(disable: 4456)    // -Wno-shadow
 #endif
