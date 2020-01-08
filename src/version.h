08a689c81e6e20593bdb08883f95dbc2d0d1722a
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2020-01-08 04:21:30 +0100

    Bump tentative release date.

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #define UPX_VERSION_HEX         0x036000        /* 03.96.00 */
 #define UPX_VERSION_STRING      "3.96"
 #define UPX_VERSION_STRING4     "3.96"
-#define UPX_VERSION_DATE        "Feb 24th 2019"
-#define UPX_VERSION_DATE_ISO    "2019-02-24"
-#define UPX_VERSION_YEAR        "2019"
+#define UPX_VERSION_DATE        "Jan 12th 2020"
+#define UPX_VERSION_DATE_ISO    "2020-01-12"
+#define UPX_VERSION_YEAR        "2020"
 
 /* vim:set ts=4 sw=4 et: */
