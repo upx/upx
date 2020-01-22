f33645f739ad75b8fc048827e33d941be975d989
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2020-01-22 23:11:13 +0100

    Post-release version bump.

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
-#define UPX_VERSION_HEX         0x036000        /* 03.96.00 */
-#define UPX_VERSION_STRING      "3.96"
-#define UPX_VERSION_STRING4     "3.96"
-#define UPX_VERSION_DATE        "Jan 23rd 2020"
-#define UPX_VERSION_DATE_ISO    "2020-01-23"
+#define UPX_VERSION_HEX         0x040000        /* 04.00.00 */
+#define UPX_VERSION_STRING      "4.00"
+#define UPX_VERSION_STRING4     "4.00"
+#define UPX_VERSION_DATE        "Jan 24th 2020"
+#define UPX_VERSION_DATE_ISO    "2020-01-24"
 #define UPX_VERSION_YEAR        "2020"
 
 /* vim:set ts=4 sw=4 et: */
