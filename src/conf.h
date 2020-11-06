d61b4ba8bb1fa2f8259028b4a26f83626febb9e8
Liam Murphy <liamjm@google.com>
2020-11-06 03:36:48 +0000

    ifdef the decision to exit or not

--- src/conf.h
+++ src/conf.h
@@ -653,12 +653,15 @@ typedef ElfLinker Linker;
 extern const char *progname;
 bool set_exit_code(int ec);
 int real_main(int, char**);
+#ifdef DO_NOT_EXIT
+void e_exit(int ec);
+#else // DO_NOT_EXIT
 #if (ACC_CC_CLANG || ACC_CC_GNUC || ACC_CC_LLVM || ACC_CC_PATHSCALE)
-// HACK HACK HACK: do this better
-void e_exit(int ec) /*__attribute__((__noreturn__))*/;
+void e_exit(int ec)__attribute__((__noreturn__));
 #else
 void e_exit(int ec);
 #endif
+#endif // DO_NOT_EXIT
 
 
 // msg.cpp
