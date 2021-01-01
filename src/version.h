c422f7968d32da710083b560ef6c1ea11b80c56e
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2021-01-01 20:45:11 +0100

    Bump version date.

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #define UPX_VERSION_HEX         0x040000        /* 04.00.00 */
 #define UPX_VERSION_STRING      "4.0.0"
 #define UPX_VERSION_STRING4     "4.00"
-#define UPX_VERSION_DATE        "Jan 24th 2020"
-#define UPX_VERSION_DATE_ISO    "2020-01-24"
-#define UPX_VERSION_YEAR        "2020"
+#define UPX_VERSION_DATE        "Jan 1st 2021"
+#define UPX_VERSION_DATE_ISO    "2021-01-01"
+#define UPX_VERSION_YEAR        "2021"
 
 /* vim:set ts=4 sw=4 et: */
