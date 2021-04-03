1f2e0d3e2a41d86b2f67ab3146d70a375bba04e6
John Reiser <jreiser@BitWagon.com>
2021-04-03 12:46:20 -0700

    Enable usual grep for function definition
    Developer convenience: no space between function name and left paren
    	modified:   compress.cpp

--- src/compress.cpp
+++ src/compress.cpp
@@ -67,12 +67,12 @@ unsigned upx_crc32(const void *buf, unsigned len, unsigned crc)
 //
 **************************************************************************/
 
-int upx_compress           ( const upx_bytep src, unsigned  src_len,
-                                   upx_bytep dst, unsigned* dst_len,
-                                   upx_callback_p cb,
-                                   int method, int level,
-                             const upx_compress_config_t *cconf,
-                                   upx_compress_result_t *cresult )
+int upx_compress( const upx_bytep src, unsigned  src_len,
+                        upx_bytep dst, unsigned* dst_len,
+                        upx_callback_p cb,
+                        int method, int level,
+                  const upx_compress_config_t *cconf,
+                        upx_compress_result_t *cresult )
 {
     int r = UPX_E_ERROR;
     upx_compress_result_t cresult_buffer;
@@ -134,10 +134,10 @@ int upx_compress           ( const upx_bytep src, unsigned  src_len,
 //
 **************************************************************************/
 
-int upx_decompress         ( const upx_bytep src, unsigned  src_len,
-                                   upx_bytep dst, unsigned* dst_len,
-                                   int method,
-                             const upx_compress_result_t *cresult )
+int upx_decompress(const upx_bytep src, unsigned  src_len,
+                         upx_bytep dst, unsigned* dst_len,
+                         int method,
+                   const upx_compress_result_t *cresult )
 {
     int r = UPX_E_ERROR;
 
@@ -177,12 +177,12 @@ int upx_decompress         ( const upx_bytep src, unsigned  src_len,
 //
 **************************************************************************/
 
-int upx_test_overlap       ( const upx_bytep buf,
-                             const upx_bytep tbuf,
-                                   unsigned  src_off, unsigned src_len,
-                                   unsigned* dst_len,
-                                   int method,
-                             const upx_compress_result_t *cresult )
+int upx_test_overlap( const upx_bytep buf,
+                      const upx_bytep tbuf,
+                            unsigned  src_off, unsigned src_len,
+                            unsigned* dst_len,
+                            int method,
+                      const upx_compress_result_t *cresult )
 {
     int r = UPX_E_ERROR;
 
