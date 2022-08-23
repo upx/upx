45e0c52ada5d157465c754293f33b9036919a2fb
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2022-08-23 05:40:44 +0200

    misc: add misc/rebuild-stubs-with-podman files

---
+++ misc/rebuild-stubs-with-podman/10-create-image.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+## vim:set ts=4 sw=4 et:
+set -e; set -o pipefail
+argv0=$0; argv0abs="$(readlink -fn "$argv0")"; argv0dir="$(dirname "$argv0abs")"
+
+# create the image from Dockerfile
+# using a rootless Podman container
+
+image=upx-stubtools-20210104-v1
+
+podman build -t "$image" -f "$argv0dir/Dockerfile" "$argv0dir"
+
+podman image list "$image"
+podman image tree "$image"
