8fe83be87f515b6385961d38646add19297b5853
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2022-10-05 16:07:12 +0200

    misc: update misc/rebuild-stubs-with-podman so that we can build the full upx program in a container

--- misc/rebuild-stubs-with-podman/20-image-run-shell.sh
+++ misc/rebuild-stubs-with-podman/20-image-run-shell.sh
@@ -6,7 +6,7 @@ argv0=$0; argv0abs="$(readlink -fn "$argv0")"; argv0dir="$(dirname "$argv0abs")"
 # run an interactive shell in the image
 # using a rootless Podman container
 
-image=upx-stubtools-20210104-v1
+image=upx-stubtools-20210104-v2
 
 flags=( -ti --read-only --rm )
 flags+=( --cap-drop=all )
@@ -26,7 +26,7 @@ podman run "${flags[@]}" "$image" bash -l
 #   # make sure that git is clean:
 #   git status .
 #   # remove stub files and make sure that they got deleted:
-#   make clean
+#   make maintainer-clean
 #   git status .
 #   # rebuild
 #   make all
