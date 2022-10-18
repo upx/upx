13276d93fa65274aa27010d27839e1f16a957fcc
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2022-10-18 21:07:48 +0200

    all: bump tentative release date

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #define UPX_VERSION_HEX         0x040000        /* 04.00.00 */
 #define UPX_VERSION_STRING      "4.0.0"
 #define UPX_VERSION_STRING4     "4.00"
-#define UPX_VERSION_DATE        "Sep 30th 2022"
-#define UPX_VERSION_DATE_ISO    "2022-09-30"
+#define UPX_VERSION_DATE        "Oct 23rd 2022"
+#define UPX_VERSION_DATE_ISO    "2022-10-23"
 #define UPX_VERSION_YEAR        "2022"
 
 /* vim:set ts=4 sw=4 et: */
