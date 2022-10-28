69ca635c4c0ba474e9ca583dbb42e3d028c4533a
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2022-10-28 10:38:03 +0200

    all: prepare for 4.0.0 release

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #define UPX_VERSION_HEX         0x040000        /* 04.00.00 */
 #define UPX_VERSION_STRING      "4.0.0"
 #define UPX_VERSION_STRING4     "4.00"
-#define UPX_VERSION_DATE        "Nov 30th 2022"
-#define UPX_VERSION_DATE_ISO    "2022-11-30"
+#define UPX_VERSION_DATE        "Oct 28th 2022"
+#define UPX_VERSION_DATE_ISO    "2022-10-28"
 #define UPX_VERSION_YEAR        "2022"
 
 /* vim:set ts=4 sw=4 et: */
