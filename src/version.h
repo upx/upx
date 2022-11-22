0af9bbb6234462b33595305675532b846eaecee3
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2022-11-22 20:51:56 +0100

    all: post-release version bump

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
-#define UPX_VERSION_HEX         0x040001        /* 04.00.01 */
-#define UPX_VERSION_STRING      "4.0.1"
-#define UPX_VERSION_STRING4     "4.01"
-#define UPX_VERSION_DATE        "Nov 16th 2022"
-#define UPX_VERSION_DATE_ISO    "2022-11-16"
+#define UPX_VERSION_HEX         0x040002        /* 04.00.02 */
+#define UPX_VERSION_STRING      "4.0.2"
+#define UPX_VERSION_STRING4     "4.02"
+#define UPX_VERSION_DATE        "Nov 22nd 2022"
+#define UPX_VERSION_DATE_ISO    "2022-11-22"
 #define UPX_VERSION_YEAR        "2022"
 
 /* vim:set ts=4 sw=4 et: */
