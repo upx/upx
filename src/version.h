e25d04fb122b04d57e010dd67ad52b48e0128c17
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2023-01-01 19:53:28 +0100

    all: bump version date

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #define UPX_VERSION_HEX         0x040002        /* 04.00.02 */
 #define UPX_VERSION_STRING      "4.0.2"
 #define UPX_VERSION_STRING4     "4.02"
-#define UPX_VERSION_DATE        "Nov 22nd 2022"
-#define UPX_VERSION_DATE_ISO    "2022-11-22"
-#define UPX_VERSION_YEAR        "2022"
+#define UPX_VERSION_DATE        "Jan 1st 2023"
+#define UPX_VERSION_DATE_ISO    "2023-01-01"
+#define UPX_VERSION_YEAR        "2023"
 
 /* vim:set ts=4 sw=4 et: */
