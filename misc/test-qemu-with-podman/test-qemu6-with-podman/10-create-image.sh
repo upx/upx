6981f7b3ae2df45de5e9b6683c30d9192ad221c7
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2023-07-08 18:14:41 +0200

    misc: add misc/test-qemu-with-podman

---
+++ misc/test-qemu-with-podman/test-qemu6-with-podman/10-create-image.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+## vim:set ts=4 sw=4 et:
+set -e; set -o pipefail
+argv0=$0; argv0abs="$(readlink -fn "$argv0")"; argv0dir="$(dirname "$argv0abs")"
+
+# create the image from Dockerfile
+# using a rootless Podman container
+
+image=upx-test-qemu6-20230708-v1
+
+podman build -t "$image" -f "$argv0dir/Dockerfile" "$argv0dir"
+
+podman image list "$image"
+echo
+podman image tree "$image"
