877911313161fbf86e2688e091b8fd1657985d19
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2023-08-08 10:38:12 +0200

    all: prepare for release

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define UPX_VERSION_HEX         0x040100        /* 04.01.00 */
 #define UPX_VERSION_STRING      "4.1.0"
 #define UPX_VERSION_STRING4     "4.10"
-#define UPX_VERSION_DATE        "Aug 3rd 2023"
-#define UPX_VERSION_DATE_ISO    "2023-08-03"
+#define UPX_VERSION_DATE        "Aug 8th 2023"
+#define UPX_VERSION_DATE_ISO    "2023-08-08"
 #define UPX_VERSION_YEAR        "2023"
