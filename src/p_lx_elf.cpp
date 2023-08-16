b9bf81d548e82fb332edb45954bca3a7eab11779
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2023-08-16 22:23:28 +0200

    src/p_lx_elf.cpp: init o_binfo to avoid crash with MSAN build
    This should be double-checked by @jreiser

--- src/p_lx_elf.cpp
+++ src/p_lx_elf.cpp
@@ -252,7 +252,7 @@ PackLinuxElf::PackLinuxElf(InputFile *f)
     : super(f), e_phnum(0), dynstr(nullptr),
     sz_phdrs(0), sz_elf_hdrs(0), sz_pack2(0), sz_pack2a(0),
     lg2_page(12), page_size(1u<<lg2_page), is_pie(0), is_asl(0),
-    xct_off(0), so_slide(0), xct_va(0), jni_onload_va(0),
+    xct_off(0), o_binfo(0), so_slide(0), xct_va(0), jni_onload_va(0),
     user_init_va(0), user_init_off(0),
     e_machine(0), ei_class(0), ei_data(0), ei_osabi(0), osabi_note(nullptr),
     shstrtab(nullptr),
