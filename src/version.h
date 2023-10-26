9c46f4653fe24a96c6723de5b69c6145d9956dac
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2023-10-26 11:09:48 +0200

    all: prepare for release

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define UPX_VERSION_HEX      0x040200 /* 04.02.00 */
 #define UPX_VERSION_STRING   "4.2.0"
 #define UPX_VERSION_STRING4  "4.20"
-#define UPX_VERSION_DATE     "Oct 22nd 2023"
-#define UPX_VERSION_DATE_ISO "2023-10-22"
+#define UPX_VERSION_DATE     "Oct 26th 2023"
+#define UPX_VERSION_DATE_ISO "2023-10-26"
 #define UPX_VERSION_YEAR     "2023"
