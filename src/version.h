cd1df6f6b15110ba2c7b560be94e947c10950e4e
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2023-10-29 15:37:13 +0100

    all: update version

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
-#define UPX_VERSION_HEX      0x040300 /* 04.03.00 */
-#define UPX_VERSION_STRING   "4.3.0"
-#define UPX_VERSION_STRING4  "4.30"
-#define UPX_VERSION_DATE     "Oct 27th 2023"
-#define UPX_VERSION_DATE_ISO "2023-10-27"
+#define UPX_VERSION_HEX      0x040201 /* 04.02.01 */
+#define UPX_VERSION_STRING   "4.2.1"
+#define UPX_VERSION_STRING4  "4.21"
+#define UPX_VERSION_DATE     "Oct 29th 2023"
+#define UPX_VERSION_DATE_ISO "2023-10-29"
 #define UPX_VERSION_YEAR     "2023"
