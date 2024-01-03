099c3d829e80488af7395a4242b318877e980da4
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2024-01-03 18:43:18 +0100

    all: prepare for release.

--- misc/podman/cross-compile-upx-ubuntu/10-create-image.sh
+++ misc/podman/cross-compile-upx-ubuntu/10-create-image.sh
@@ -6,10 +6,10 @@ argv0=$0; argv0abs="$(readlink -fn "$argv0")"; argv0dir="$(dirname "$argv0abs")"
 # create the image from Dockerfile
 # using a rootless Podman container
 
-# NOTE: this image is based on rebuild-stubs/upx-stubtools-20221212-v6,
+# NOTE: this image is based on rebuild-stubs/upx-stubtools-20221212-v7,
 #   so you have to create that image first
 # WARNING: we install many packages, so the resulting image needs A LOT of disk space!
-image=upx-cross-compile-ubuntu2204-20230721-v2
+image=upx-cross-compile-ubuntu2204-20230721-v3
 [[ $1 == --print-image ]] && echo "$image" && exit 0
 
 podman build -t "$image" -f "$argv0dir/Dockerfile" "$argv0dir"
