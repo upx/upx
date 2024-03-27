5bef96806860382395d9681f3b0c69e0f7e853cf
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2024-03-27 13:57:30 +0100

    all: prepare for release

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define UPX_VERSION_HEX      0x040203 /* 04.02.03 */
 #define UPX_VERSION_STRING   "4.2.3"
 #define UPX_VERSION_STRING4  "4.23"
-#define UPX_VERSION_DATE     "Mar 12th 2024"
-#define UPX_VERSION_DATE_ISO "2024-03-16"
+#define UPX_VERSION_DATE     "Mar 27th 2024"
+#define UPX_VERSION_DATE_ISO "2024-03-27"
 #define UPX_VERSION_YEAR     "2024"
